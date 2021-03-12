253093af547a77cb37db94187e305a9862c2f635
JarKz <76003928+JarKz@users.noreply.github.com>
2021-03-12 17:19:41 +0300

    Updated file cfg.py

--- cfg.py
+++ cfg.py
@@ -1,10 +1,2 @@
-TOKEN = '930dd01ca0b18645bcda794b61f0fd3b7261e68dcbbf47ac4d5a76a58b65eafd23c2d5a99548592c3aa52'
+TOKEN = '___'
 ADMIN_ID = ''
-DATABASE = {
-    'drivename':'postgres',
-    'host':'localhost',
-    'port':'5432',
-    'username':'postgres',
-    'password':'Strannik003dobro_',
-    'database':'test'
-}
